a718b9a659746ca8d113ac4fb621ea78c5dce2cd
julian98789 <julian_gomez69222@elpoli.edu.co>
2024-07-14 14:50:49 -0500

    Agregue dependencias

---
+++ src/test/java/Chat/Spring_web_socket/SpringWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package Chat.Spring_web_socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
