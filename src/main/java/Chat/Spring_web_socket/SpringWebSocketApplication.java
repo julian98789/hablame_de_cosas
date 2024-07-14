85bb41ae5d7341777e0fb61d7f374f513221c209
julian98789 <julian_gomez69222@elpoli.edu.co>
2024-07-14 14:50:49 -0500

    implemente una interface para el usuario

---
+++ src/main/java/Chat/Spring_web_socket/SpringWebSocketApplication.java
@@ -0,0 +1,13 @@
+package Chat.Spring_web_socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketApplication.class, args);
+	}
+
+}
