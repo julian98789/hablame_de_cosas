17959d2d502717fe8e8a4cf49ef44990cb2f5adb
julian98789 <julian_gomez69222@elpoli.edu.co>
2024-08-04 16:35:38 -0500

    arreglo del diseño responsive

--- Front End/src/components/sideBarMovile/SideBarMovile.jsx
+++ Front End/src/components/sideBarMovile/SideBarMovile.jsx
@@ -12,9 +12,9 @@ export const SideBarMovile = ({state,setState}) => {
             ${state ? 'translate-x-0' : '-translate-x-full'}   z-20
 
         `} >
-            <div className=" w-80 h-screen flex flex-col bg-[hsl(209,92%,5%)] ">  
+            <div className=" w-80 h-screen flex flex-col justify-center bg-[hsl(209,92%,5%)] ">  
                 <div className="w-full h-14"></div>
-                <div className="flex text-3xl font-macondo font-bold gap-2 mt-10 ml-10 text-[rgb(221,237,115,255)] text-aura-parentesis">/*</div>
+                <div className="flex text-3xl font-macondo font-bold gap-2  ml-10 text-[rgb(221,237,115,255)] text-aura-parentesis">/*</div>
                 <div className="flex flex-col  items-center">
                 <a
                         href="https://www.linkedin.com/in/juliangomez060/"
